bd62bae123047473c5b6bb2378026ff7d94072a8
Poonam Godse <karchepoonam.b@gmail.com>
2023-05-06 18:40:00 -0700

    03_tyeof.js is completed
    03_tyeof.js is completed

---
+++ 03_typeof.js
@@ -0,0 +1,38 @@
+
+console.log("---before initialisation---");
+var bankName; // Variable Declaration
+console.log(bankName); // undefined
+console.log("---after initialisation---");
+bankName = "SBI Bank"; // Variable Initialization
+console.log("BankName:",bankName);
+var typeofbankName = typeof bankName;
+console.log("typeof bankName:",typeof bankName);
+console.log("------------------------------------------");
+
+console.log("----initialisation of variable marks----");
+var marks=90;
+console.log("Marks:", marks);
+var typeofmarks = typeof marks;
+console.log("typeof marks:",typeof marks);
+
+console.log("---------------------------");
+console.log("---before initialisation---");
+var isWorking; // Variable Declaration
+console.log(isWorking); // undefined
+console.log("---after initialisation---");
+isWorking = true; // Variable Initialization
+console.log("isWorking:",isWorking);
+var typeofisWorking = typeof isWorking;
+console.log("typeof isWorking:",typeof isWorking);
+console.log("-----------------------------------------");
+
+console.log("----initialisation of variable totalCount----");
+var totalCount="Hundred and Seven";
+console.log("totalCount:", totalCount);
+var typeoftotalCount = typeof totalCount;
+console.log("typeof totalCount:",typeof totalCount);
+totalCount = 107; // Variable update
+console.log("totalCount:", totalCount);
+var typeoftotalCount = typeof totalCount;
+console.log("typeof totalCount:",typeof totalCount);
+
