a8535adfcf1a6a4b8c69fee08ba3cef4424f7b59
Poonam Godse <karchepoonam.b@gmail.com>
2023-05-11 08:45:39 -0700

    02_swapvariable is done
    02_swapvariable is done

---
+++ 02_swapvariables.js
@@ -0,0 +1,22 @@
+console.log("----before swap----");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("sweety: ", sweety, " cutie: ", cutie);
+console.log("------after swap-----");
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("sweety:", sweety, " cutie:", cutie);
+console.log("------------------------------");
+
+console.log("------before swap--------");
+var num1 = 100;
+var num2 = 200;
+var num3 =300;
+console.log( "num1: ", num1, " num2: ", num2,"num3:",num3);
+console.log('--------after swap---------');
+var temp = num1;
+num1 = num2;
+num2=num3;
+num3=temp;
+console.log("num1:", num1, " num2:", num2,"num3:", num3);
---
+++ 03_funAssign01.js
@@ -0,0 +1,41 @@
+
+function fullName(firstName,lastName) {
+    console.log("fullName:",firstName,lastName);
+}
+fullName("Poonam", "Godse");
+
+function address(address) {
+    console.log("Address:",address);
+}
+address("Vaibhavnagar,Pimpri,Pune");
+
+console.log("------Personal Details--------");
+function personalDetails(firstName,lastName,collegeName) {
+           console.log("My Personal Detail is");
+           console.log("firstName:",firstName);
+           console.log("lastName:",lastName);
+           console.log("collegeName:",collegeName);
+}
+personalDetails("Poonam","Godse","PVG COET,PUNE");
+
+console.log("---------swapValueDude-----------------"); 
+function swapValueDude(argOne, argTwo){ // Arguments: valueOne =100  valueTwo = 200 var valueThree;
+    console.log("before Swap: ", argOne, argTwo);
+    var temp = argOne;
+     argOne = argTwo; 
+    argTwo = temp;
+    console.log("After Swap: ", argOne, argTwo);
+}
+swapValueDude("Virat", "Anushka"); 
+swapValueDude(1000, 2000);
+
+
+console.log("---------AddThreeValue-----------------"); 
+
+function addThreeValue(valueOne,valueTwo,valueThree){
+    var result=valueOne+valueTwo+valueThree;
+    console.log("additionIs:",result);
+}
+addThreeValue(10.23,600,40);
+addThreeValue("Hello", "Good", "Morning");
+
