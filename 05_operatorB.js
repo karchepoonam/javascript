cdc7c010ec9d1881f9ab2e895f24c37c33c83105
Poonam Godse <karchepoonam.b@gmail.com>
2023-05-19 14:36:44 -0700

    05_operatorB.js is ready
    05_operatorB.js is ready

--- 05_operatorB.js
+++ 05_operatorB.js
@@ -1,35 +1,37 @@
+var greaterNumber = function number(num1, num2) {
+  var result = num1 >= num2 ? `${num1}is greater` : `${num2}is greater`;
+  console.log(result);
+};
 
+greaterNumber(10, -10);
+greaterNumber(800, 899);
 
-var greaterNumber=function num (num1,num2) {
-    var result=num1>=num2? `$(num1)is greater`:`$(num2)is greater`;
-    console.log(result);
-}
+var isEvenOrOddNum = function (num) {
+  var result = num % 2 == 0 ? `${num}is even` : `${num}is odd`;
+  return result;
+};
+var result = isEvenOrOddNum(29);
+var res = result == true ? "29 is even" : "29 is odd";
+console.log(`${res}`);
+var result = isEvenOrOddNum(44);
+var res = result == true ? "44 is even" : "44 is odd";
+console.log(`${res}`);
 
-greaterNumber(10,-10);
-greaterNumber(800,899);
+var result = isEvenOrOddNum(0);
+var res = result == true ? "0 is even" : "0 is odd";
+console.log(`${res}`);
+var result = isEvenOrOddNum(101);
+var res = result == true ? "101 is even" : "101 is odd";
+console.log(`${res}`);
 
-
-var isEvenOrOddNum=function (num) {
-    var result=num%2==0? `$(num)is even`:`$(num)is odd`; 
-    return result;
-}
- var result=isEvenOrOddNum(29);
-var res=(result==true) ? "29 is even":"29 is odd";
-console.log('${res}');
-
-
-console.log(``);
-isEvenOrOddNum(29);
-
-
-var wordlength=function(word){
-    var len=word.length;
-    var result=len%2==0?"Even":"odd";
-    return result;
-}
-var returnValue=wordlength("JavaScript");
+var wordlength = function (word) {
+  var len = word.length;
+  var result = len % 2 == 0 ? "Even" : "odd";
+  return result;
+};
+var returnValue = wordlength("JavaScript");
 console.log(`JavaScript has ${returnValue}`);
-var returnValue=wordlength("Developer");
+var returnValue = wordlength("Developer");
 console.log(`Developer has ${returnValue}`);
-var returnValue=wordlength("Google");
+var returnValue = wordlength("Google");
 console.log(`Google has ${returnValue}`);
