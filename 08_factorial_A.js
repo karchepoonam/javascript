b6b93ee18774a60f03756129fbe1dd3c502279c6
Poonam Godse <karchepoonam.b@gmail.com>
2023-05-25 05:09:26 -0700

    08_factorial_A.js is completed
    08_factorial_A.js is completed

---
+++ 08_factorial_A.js
@@ -0,0 +1,48 @@
+let factorialOfNum = 1
+for (let index = 5; index >= 1; index--) { 
+    factorialOfNum = factorialOfNum * index; 
+}
+console.log(`Factorial of 5 is:`, factorialOfNum)
+
+let factorialOfNum1 = 1
+for (let index = 3; index >= 1; index--) { 
+    factorialOfNum1 = factorialOfNum1 * index; 
+}
+console.log(`Factorial of 3 is:`, factorialOfNum1)
+
+
+let factorialOfNumbs=null;
+for (let index = null; index >= 1; index--) { 
+    factorialOfNumbs = factorialOfNumbs * index; 
+}
+console.log(`invalid data:`, factorialOfNumbs)
+
+let factorialOfNums = 1
+for (let index = 8; index >= 1; index--) { 
+    factorialOfNums = factorialOfNums * index; 
+}
+console.log(`Factorial of 8 is:`, factorialOfNums)
+
+let factorialOfNumbss;
+for (let index ; index >= 1; index--) { 
+    factorialOfNumbss = factorialOfNumbss * index; 
+}
+console.log(`invalid data:`, factorialOfNumbss)
+
+let factorialOfNumber = 1
+for (let index = 9; index >= 1; index--) { 
+    factorialOfNumber = factorialOfNumber * index; 
+}
+console.log(`Factorial of 9 is:`, factorialOfNumber)
+
+
+let factorialOfNumb = 1
+for (let index = 0; index >= 1; index--) { 
+    factorialOfNumb = factorialOfNumb * index; 
+}
+console.log(`Factorial of 0 is:`, factorialOfNumb)
+
+
+
+
+
