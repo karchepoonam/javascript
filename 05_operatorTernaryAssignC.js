550375f95301958ffa28b04ca4e95d8d7251b2f2
Poonam Godse <karchepoonam.b@gmail.com>
2023-05-28 03:22:44 -0700

    05_operatorTernoryAssignc.js is done
    05_operatorTernoryAssignc.js is done

---
+++ 05_operatorTernaryAssignC.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+var boyName = "Bill gates";
+var age = 25
+var gender = "male";
+
+var result =  (gender=="male" && age>=21)  ? "Hey Bill Gates you are eligible for marriage" : "No you are not eligible";
+console.log(`${result}`);
+
+var boyName = "Steve Jobs";
+var age = 17
+var gender = "male";
+
+var result =  (gender=="male" && age>=21)  ? "Hey Steve Jobs you are eligible for marriage" : "Hey Steve Jobs you are not eligible for marriage";
+console.log(`${result}`);
+console.log("---------------------------------------------------------------------------------");
+var girlName = "Jenifer";
+var age = 16
+var gender = "female";
+
+var result =  (gender=="female" && age>=18)  ? "Hey Jenifer you are eligible for marriage" : "Hey Jenifer you are not eligible for marriage";
+console.log(`${result}`);
+
+var girlName = "Malinda Gates";
+var age = 27
+var gender = "female";
+
+var result =  (gender=="female" && age>=18)  ? "Hey Malinda you are eligible for marriage" : "Hey Malinda you are not eligible for marriage";
+console.log(`${result}`);
