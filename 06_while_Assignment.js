46b9eeec2a99d1de96817206eef556da605882e8
Poonam Godse <karchepoonam.b@gmail.com>
2023-05-28 03:21:38 -0700

    submitting 06_while_Assignment.js
    submitting 06_while_Assignment.j

---
+++ 06_while_Assignment.js
@@ -0,0 +1,66 @@
+console.log("1.To print number from 5 to 15");
+// WAP to print number from 5 to 15
+let i = 5; // initialization
+while (i <= 15) {
+  // condition 0<=15
+  console.log(i);
+  i = i + 1;
+}
+
+console.log("2.To print number from 50 to 40");
+// WAP to print number from 50 to 40
+let k = 50;
+while (k >= 40) {
+  console.log(k);
+  k--;
+}
+
+// WAP To print first 15 odd numbers
+console.log("3. To print first 15 odd numbers");
+
+let j = 0; // initialization
+while (j <= 15) {
+  // condition
+  if (j % 2 == 1) {
+    console.log(j);
+  }
+  j++; // update expression
+}
+
+// WAP To print first 10 even numbers
+console.log("4. To print first 10 even numbers");
+
+let l = 0; // initialization
+while (l <= 10) {
+  // condition
+  if (l % 2 == 0) {
+    console.log(l);
+  }
+  l++; // update expression
+}
+
+// WAP to print table of 5
+console.log("5.To print table of 5");
+let n = 5; // initialization
+while (n <= 50) {
+  // condition
+  console.log(n);
+  n = n + 5;
+}
+
+// WAP to print table of 10
+console.log("6.To print table of 10");
+let m = 10; // initialization i = 1   2  3
+while (m <= 100) {
+  // condition 0<=10
+  console.log(m); // 0  1 2
+  m = m + 10;
+}
+
+// WAP To print table of 10 in reverse order
+console.log("7.To print table of 10 in reverse order");
+let p = 100; // initialization
+while (p >= 10) {
+  console.log(p);
+  p = p - 10;
+}
