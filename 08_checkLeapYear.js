a5e48ceb0bc37a3213e683da804f3f47760177f5
Poonam Godse <karchepoonam.b@gmail.com>
2023-06-13 11:26:40 -0700

    08_checkLeapYear.js is completed
    08_checkLeapYear.js is completed

---
+++ 08_checkLeapYear.js
@@ -0,0 +1,23 @@
+function checkLeapYear(year) {
+
+
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+
+const year = ('Enter a year:');
+
+checkLeapYear(2020);
+checkLeapYear(1999);
+checkLeapYear(1600);
+checkLeapYear(2022);
+checkLeapYear(1945);
+checkLeapYear(null);
+checkLeapYear("Twenty Twenty");
+checkLeapYear(undefined);
+checkLeapYear(NaN);
+checkLeapYear(1750);
